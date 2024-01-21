e2964192f4cb3b3f92021346e48702cb3a6b1115
fcarella <fred.carella@gmail.com>
2024-01-21 11:26:46 -0500

    Initial commit

---
+++ src/main/java/com/example/FredCarellaCsd226HtmxTutorial/FredCarellaCsd226HtmxTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.FredCarellaCsd226HtmxTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FredCarellaCsd226HtmxTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FredCarellaCsd226HtmxTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FredCarellaCsd226HtmxTutorial/FredCarellaCsd226HtmxTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FredCarellaCsd226HtmxTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FredCarellaCsd226HtmxTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
