3d9e105f9b4bb667b6903a8a330c430e5ca55fe6
Fred Carella <fred.carella@gmail.com>
2024-01-21 20:08:32 -0500

    commit

--- src/main/java/com/example/FredCarellaCsd226HtmxTutorial/GreetController.java
+++ src/main/java/com/example/FredCarellaCsd226HtmxTutorial/GreetController.java
@@ -1,10 +1,16 @@
 package com.example.FredCarellaCsd226HtmxTutorial;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import org.springframework.web.reactive.result.view.Rendering;
+
 
 //@SpringBootApplication
 @RestController
@@ -20,6 +26,12 @@ public class GreetController {
         return "Hello " + values.getValue() + "!";
     }
 
+    @GetMapping(path = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Rendering> stream() {
+        return Flux.interval(Duration.ofSeconds(5)).map(value -> Rendering.view("time")
+                .modelAttribute("value", value)
+                .modelAttribute("time", System.currentTimeMillis()).build());
+    }
 
     static class Greeting {
         private String value;
